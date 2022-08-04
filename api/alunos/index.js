06a9e109d78c3fefcfb74678edbbd54bea711d57
renato_sans <renato@datacopy.com.br>
2022-08-04 14:04:58 -0300

    Ajustes

--- api/alunos/index.js
+++ api/alunos/index.js
@@ -1,11 +1,19 @@
 
+function getAlunos(req, res) {
+	res.send('Listagem de alunos');
+}
+
+function insertAluno(req, res) {
+	res.send('Registro salvo com sucesso');
+}
+
 module.exports = {
 	default: (req, res) => {
 		switch (req.method) {
 			case "GET": return getAlunos(req, res)
-			case "POST": return insertAlunos(req, res)
+			case "POST": return insertAluno(req, res)
 		}
 	},
     get: (req, res) => getAlunos(req, res),
-    post: (req, res) => insertAlunos(req, res)
+    post: (req, res) => insertAluno(req, res)
 }
--- api/professores/index.js
+++ api/professores/index.js
@@ -1,11 +1,19 @@
 
+function getProfessores(req, res) {
+	res.send('Listagem de professores');
+}
+
+function insertProfessor(req, res) {
+	res.send('Registro salvo com sucesso');
+}
+
 module.exports = {
 	default: (req, res) => {
 		switch (req.method) {
 			case "GET": return getProfessores(req, res)
-			case "POST": return insertProfessores(req, res)
+			case "POST": return insertProfessor(req, res)
 		}
 	},
     get: (req, res) => getProfessores(req, res),
-    post: (req, res) => insertProfessores(req, res)
+    post: (req, res) => insertProfessor(req, res)
 }
