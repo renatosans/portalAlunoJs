b130da3211c9a7a6fd843664292a850f04ab4821
renato_sans <renato@datacopy.com.br>
2022-08-04 20:14:21 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ export default function App() {
           <div id="panel"></div>
         </header>
         <main className="main">
-            <ClubList></ClubList>
         </main>
         <footer className="footer">
           <div id="bottom-content"></div>
