541b8ad9a80ba83fff036027c6e79b1150a05ff5
renato_sans <renato@datacopy.com.br>
2022-08-04 14:06:18 -0300

    Ajustes

--- api/index.js
+++ api/index.js
@@ -1,18 +1,18 @@
 const mysql = require('mysql2');
 const express = require('express');
-// const { nextApi, nextRouter } = require('express-next-api');
+const { nextApi, nextRouter } = require('express-next-api');
 
 
 const port = 3000;
 const app = express();
 
-app.use("/", express.static('Public'));
+app.use("/", express.static('../Public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use(nextApi({ base: '/api', directory: '../', options: {caseSensitive: false} }))
 
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
-
 
 /*
 const mysqlConnection = mysql.createConnection({ host: 'localhost', user: 'root', password: 'p@ssw0rd', port: '3306', database: 'portal_aluno' });
