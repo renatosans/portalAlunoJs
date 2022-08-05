9742bd82bc7ca5c89930e167f8c952c9c5333b46
renato_sans <renato@datacopy.com.br>
2022-08-05 10:07:41 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,13 @@ export default function App() {
   return (
       <>
         <header className="header">
-          <div id="panel"></div>
+          <img src="images\logo.png" style={{"width":"200px", "height":"200px", "margin": "50px"}} />
         </header>
         <main className="main">
           <TeacherList></TeacherList>
         </main>
         <footer className="footer">
-          <div id="bottom-content"></div>
+          <div id="panel"></div>
         </footer>
       </>
   )
