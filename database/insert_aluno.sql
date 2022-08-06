506218a71bd6f6f2e61601f823a5fe09bdc31383
renato_sans <renato@datacopy.com.br>
2022-08-06 05:51:29 -0300

    Inclusão de massa de dados

---
+++ database/insert_aluno.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO portal_aluno.aluno (`rg`,`nome`,`serie`,`classe`) VALUES ('1111','Gabrielle Lins da Costa','8','2 B');
+INSERT INTO portal_aluno.aluno (`rg`,`nome`,`serie`,`classe`) VALUES ('2222','Joao Carlos Cardoso','8','2 B');
+INSERT INTO portal_aluno.aluno (`rg`,`nome`,`serie`,`classe`) VALUES ('3333','Vicente Santos da Silva','5','3 C');
+INSERT INTO portal_aluno.aluno (`rg`,`nome`,`serie`,`classe`) VALUES ('4444','Sarah Carvalho Mendonça','5','3 C');
