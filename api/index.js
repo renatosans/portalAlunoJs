490a362df7df454517373be11c2666ab9787fdef
renato_sans <renato@datacopy.com.br>
2022-08-06 07:43:20 -0300

    Ajustes

--- api/index.js
+++ api/index.js
@@ -6,10 +6,10 @@ const { nextApi, nextRouter } = require('express-next-api');
 const port = 3000;
 const app = express();
 
-app.use("/", express.static('../Public'));
+app.use("/", express.static('../dist'));  // necessário efetuar o build do frontend
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
-app.use(nextApi({ base: '/api', directory: 'routes', options: {caseSensitive: false} }))
+app.use(nextApi({ base: '/api/routes', directory: 'routes', options: {caseSensitive: false} }))
 
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
