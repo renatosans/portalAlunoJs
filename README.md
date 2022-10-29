f819863bbd87197ad5f37a2827627b5538a6dd32
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-29 13:28:36 -0300

    Ajustes

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # portalAlunoJs
-Portal do aluno
+Portal do Aluno
+
+Para rodar o projeto baixe as dependencias tanto do backend quanto do frontend
+ - NPM INSTALL
+ - CD API
+ - NPM INSTALL
+
+Para criar o banco de dados execute o script contido em   /api/database/create.sql
+Opcionalmente criar o banco através do comando  PRISMA DB PUSH
+
+
+NPM RUN DEV para compilar e iniciar o Express  ( frontend via static do Express , pasta '../dist' )
--- api/index.js
+++ api/index.js
@@ -1,4 +1,3 @@
-const cors = require('cors');
 const express = require('express');
 // const { createRouter } = require('express-file-routing');
 const { nextApi, nextRouter } = require('express-next-api');
@@ -7,16 +6,21 @@ const { nextApi, nextRouter } = require('express-next-api');
 const port = 3000;
 const app = express();
 
-app.use("/", express.static('../dist'));  // necessário efetuar o build do frontend
+app.use("/", express.static('../dist'));  // necessário efetuar o build do frontend   Ex.: NPM RUN BUILD
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
 // setHeader('Access-Control-Allow-Origin', '*')
-app.use(cors())
 
 // createRouter(app, { directory: apiDirectory, additionalMethods: null } )
 app.use(nextApi({ base: '/api/routes', directory: 'routes', options: {caseSensitive: false} }))
 
+
+// Redireciona para o react-router rotas não encontradas no Express
+// No react-router ( frontend ) redirecionar rotas inexistentes para a raiz
+app.get('*', (req, res) => {
+    res.sendFile('index.html', { root: '../dist' })
+})
 
 // inicia a API escutando na porta 3000
 app.listen(port, () => console.log('Express escutando chamadas na porta ' + port));
