d66cacfcde679542695acf4dddf07b471d25b715
Renato <42098236+renato-sanseverino@users.noreply.github.com>
2022-10-29 14:42:08 -0300

    Ajustes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ export default function App() {
   return (
     <>
       // React Router doesn´t seem to work on vercel
-      https://stackoverflow.com/questions/64815012/why-does-react-router-not-works-at-vercel
       // Possible fix :   add  vercel config file
       <Routes>
         <Route path="*" element={<Navigate to="/" replace />} />
